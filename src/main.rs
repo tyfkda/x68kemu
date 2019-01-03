c4711b8f770c6bb901e40c0c2aab57b636ecfbce
tyfkda <tyfkda@gmail.com>
2019-01-03 21:31:38 +0900

    Read IPL rom data from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let res = fs::read("X68BIOS/IPLROM.DAT");
+    match res {
+        Result::Ok(data) => {
+            println!("len={:?}", data.len());
+            println!("[0,1,2,3]={:?},{:?},{:?},{:?}", data[0x10000], data[0x10001], data[0x10002], data[0x10003]);
+        },
+        Result::Err(err) => {
+            panic!(err);
+        }
+    }
 }
