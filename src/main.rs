82707414eb5fbc8d384e76563923bdc075cb589d
tyfkda <tyfkda@gmail.com>
2019-01-17 23:09:55 +0900

    Show window using SDL2

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 use std::fs;
 
+mod app;
 mod cpu;
 mod types;
 mod x68k;
 
-use self::x68k::{X68k};
+use self::app::{App};
 
 fn main() {
     match fs::read("X68BIOSE/IPLROM.DAT") {
         Result::Ok(ipl) => {
-            let mut x68k = X68k::new(ipl);
-            loop {
-                x68k.update(10000);
-            }
+            let mut app = App::new(ipl);
+            app.run();
         },
         Result::Err(err) => {
             panic!(err);
