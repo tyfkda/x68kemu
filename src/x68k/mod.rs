4689fc4ad7bd63d29cc702b11b20dcdb93f73ff1
tyfkda <tyfkda@gmail.com>
2019-01-26 15:01:33 +0900

    Add fdc module

--- src/x68k/mod.rs
+++ src/x68k/mod.rs
@@ -1,5 +1,7 @@
 mod bus;
+mod fdc;
 mod x68k;
 
 pub use self::bus::{Bus};
+pub use self::fdc::{Fdc};
 pub use self::x68k::{X68k};
--- src/x68k/x68k.rs
+++ src/x68k/x68k.rs
@@ -1,19 +1,26 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use super::bus::{Bus};
+use super::fdc::{Fdc};
 use super::super::cpu::{Cpu};
 use super::super::types::{Byte};
 
 pub struct X68k {
     cpu: Cpu<Bus>,
+    //fdc: Rc<RefCell<Fdc>>,
 }
 
 impl X68k {
     pub fn new(ipl: Vec<Byte>) -> X68k {
-        let bus = Bus::new(ipl);
+        let fdc = Rc::new(RefCell::new(Fdc::new()));
+        let bus = Bus::new(ipl, fdc.clone());
         let mut cpu = Cpu::new(bus);
         cpu.reset();
 
         let x68k = X68k {
             cpu,
+            //fdc,
         };
         x68k
     }
